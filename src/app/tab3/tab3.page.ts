4ddeef0c6b08e9166b7434f835221612d8a9bc8a
badilla257 <darielgie.badilla@evsu.edu.ph>
2022-04-19 00:20:47 +0800

    My Activity 2

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  contactlist  = [
+    {id: 1, name: 'Dariel Gie Badilla', email: 'darielgiebadilla8@gmail.com',number: '09704536995'},
+    {id: 1, name: 'Aika Melendez', email: 'aikamelendez@gmail.com',number: '09063456789'},
+    {id: 1, name: 'Celine Dion', email: 'dionceline@gmail.com',number: '09087934345'},
+
+  ]
+
+    
+  
 
 }
